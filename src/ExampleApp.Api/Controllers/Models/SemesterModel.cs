06c433186d33ec57bb4ef494ad28a3fb553d7c14
Villagran Gustavo <gusvillagran@gmail.com>
2024-04-12 10:37:29 -0300

    Task 4 - Change the format of the listing of current Courses -Created the SemesterModel, GetSemesterActiveOnDateQuery and GetSemesterActiveOnDateHandler -Modified the CourseModel and CoursesController

---
+++ src/ExampleApp.Api/Controllers/Models/SemesterModel.cs
@@ -0,0 +1,5 @@
+﻿using ExampleApp.Api.Domain.Academia;
+using System.Text.Json.Serialization;
+namespace ExampleApp.Api.Controllers.Models;
+
+public record SemesterModel([property: JsonPropertyName("key")] string Id, [property: JsonPropertyName("name")] string Description, [property: JsonPropertyName("startDate")] DateOnly Start, [property: JsonPropertyName("endDate")] DateOnly End, List<CourseModel> Courses);
